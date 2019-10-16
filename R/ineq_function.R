69d7a22d8f3c3be753b68cc44c7087cc1b67d97e
Louis Penrod <louismpenrod@gmail.com>
2019-10-16 12:10:13 -0400

    Original Git build from R project

---
+++ R/ineq_function.R
@@ -0,0 +1,50 @@
+#' An Inequality Function
+#'
+#' This function allows the user to count the number of values/rows
+#' that match the inequality.
+#' @param x Data to pass through function. Vector with multiple values
+#' @param val Value to evaluate with inequality
+#' @param eq inequality symbol as a character.
+#'     Options are ">", "<", ">=", "<=", or "=".
+#' @export
+#' @examples
+#' ### Create Test data
+#' a<-seq(1,5,by=1)
+#' b<-seq(1,10,by=2)
+#' c<-seq(1,15,by=3)
+#' data<-data.frame(a,b,c)
+#'
+#' #for vector:
+#' ineq(a,val=3,eq="<")
+#'
+#' #for dataframe:
+#' apply(data,2,function(x) ineq(x,val=4,eq=">"))
+#' @return Value of observations that fit the condition given.
+
+ineq<-function(x,val,eq){
+  if(eq==">"){
+    sm<-length(x[x>val])
+    return(sm)
+  }
+  else if(eq=="<"){
+    sm<-length(x[x<val])
+    return(sm)
+  }
+  else if(eq==">="){
+    sm<-length(x[x>=val])
+    return(sm)
+  }
+  else if(eq=="<="){
+    sm<-length(x[x<=val])
+    return(sm)
+  }
+  else if(eq=="="){
+    sm<-length(x[x==val])
+    return(sm)
+  }
+  else {
+    return(NA)
+  }
+}
+#devtools::document()
+
---
+++ R/reextent_function.R
@@ -0,0 +1,51 @@
+#' A function to alter the extent of rasters
+#'
+#' This function shifts the extent of a raster from
+#'  -180:180 to 0:360 and vice versa
+#'
+#' @param ras Raster object to shift the extent
+#' @param method Numerical value to indicate direction of
+#' extent change. '1' shifts from -180:180 to 0:360. '2' shifts from
+#' 0:360 to -180:180.
+#' @export
+#' @examples
+#' ### Create Test data
+#' r<-raster()
+#' values(r) <- c(rep(1:360,length.out=64800/2),rep(1:360,length.out=64800/2))
+#' plot(r)
+#' # convert r from -180:180 to 0:360
+#' out <- reextent(r, method = 1)
+#' plot(out)
+#' # convert back to -180:180
+#' out2 <- reextent(out, method = 2)
+#' plot(out2)
+#'
+#' @return Shifted raster
+
+
+reextent <- function(ras,method){
+  if(method==1){
+    #create extents to cut map into two chunks
+    ext1 <- raster::extent(-180, 0, -90, 90)
+    ext2 <- extent(0, 180, -90, 90)
+    #crop raster
+    r1<-crop(ras,ext1)
+    r1 <- shift(r1, 360)
+    r2<-crop(ras,ext2)
+    #merge raster after shift
+    rasout <- raster::merge(r1, r2, overlap = FALSE)
+    return(rasout)
+  }
+  if(method==2){
+    #create extents to cut map into two chunks
+    ext1 <- raster::extent(0, 180, -90, 90)
+    ext2 <- extent(180, 360, -90, 90)
+    #crop raster
+    r1<-crop(ras,ext1)
+    r2<-crop(ras,ext2)
+    r2 <- shift(r2, -360)
+    #merge raster after shift
+    out <- raster::merge(r1, r2, overlap = FALSE)
+    return(out)
+  }
+}
