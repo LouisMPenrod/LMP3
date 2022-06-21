167cca040ae51b5ae2208b571ae77133fa561a92
Louismp1223 <louismpenrod@gmail.com>
2022-06-21 10:08:35 -0400

    Correct factor_sort_function function calls with :: and move packages from Depends to Imports in DESCRIPTION

--- R/factor_sort_function.R
+++ R/factor_sort_function.R
@@ -15,74 +15,69 @@
 #'
 #'
 factor_sort <- function(data,col){
-  require(shiny)
-  require(sortable)
-  require(rclipboard)
-  require(tidyverse)
-
   # Ensure column exists in data
-  t <- try(data %>% pull(var = !!enquo(col)), silent = TRUE)
+  t <- try(data %>% dplyr::pull(var = !!dplyr::enquo(col)), silent = TRUE)
   if("try-error" %in% class(t)) stop("column provided does not exist")
 
   # enquo column name
-  column_name <- enquo(col)
+  column_name <- dplyr::enquo(col)
 
   # Ensure column provided is either a factor or a character
-  if(!is.factor(data %>% pull(var = !!column_name)) &
-     !is.character(data %>% pull(var = !!column_name))){
+  if(!is.factor(data %>% dplyr::pull(var = !!column_name)) &
+     !is.character(data %>% dplyr::pull(var = !!column_name))){
     stop("column must be either a factor or a character")
   }
 
   # Ensure there is more than one unique value in the column
-  if((data %>% pull(var = !!column_name) %>% unique() %>% length())==1){
+  if((data %>% dplyr::pull(var = !!column_name) %>% unique() %>% length())==1){
     warning("only one unique value exists in column provided")
   }
 
   # Extract levels
   labels <- as.list(levels(factor(data %>%
-                                    pull(var = !!column_name))))
+                                    dplyr::pull(var = !!column_name))))
 
   # Set up soratable rank_list
-  rank_list_basic <- rank_list(
+  rank_list_basic <- sortable::rank_list(
     text = "Drag the items to the desired order",
     labels = labels,
     input_id = "rank_list_basic"
   )
 
   # Start app
-  shinyApp(
-    ui = fluidPage(
-      fluidRow(
+  shiny::shinyApp(
+    ui = shiny::fluidPage(
+      shiny::fluidRow(
 
         # main page
-        column(10, offset = 1,
-               tabPanel(
-                 "Default",
-                 tags$h1(str_glue("factor_sort for {quo_name(column_name)}")),
-                 rank_list_basic,
-                 br(),
-                 # tags$b("Result"),
-                 # verbatimTextOutput("results_basic"),
-                 # br(),
-                 tags$b("Releveling Code"),
-                 verbatimTextOutput("code")
-               )
+        shiny::column(10, offset = 1,
+                      shiny::tabPanel(
+                        "Default",
+                        tags$h1(stringr::str_glue("factor_sort for {dplyr::quo_name(column_name)}")),
+                        rank_list_basic,
+                        shiny::br(),
+                        # tags$b("Result"),
+                        # shiny::verbatimTextOutput("results_basic"),
+                        # shiny::br(),
+                        tags$b("Releveling Code"),
+                        shiny::verbatimTextOutput("code")
+                      )
         )
 
 
 
       ),
 
-      rclipboardSetup(),
+      rclipboard::rclipboardSetup(),
 
       # copy buttom
-      fluidRow(
-        column(6,offset = 1,uiOutput("clip")
+      shiny::fluidRow(
+        shiny::column(6,offset = 1,shiny::uiOutput("clip")
         )),
 
       # close button
-      fluidRow(
-        column(3, offset = 9,actionButton("close","Done", class = "btn-warning")
+      shiny::fluidRow(
+        shiny::column(3, offset = 9,shiny::actionButton("close","Done", class = "btn-warning")
         ))
 
     ),
@@ -97,21 +92,21 @@ factor_sort <- function(data,col){
       # })
 
       # Main text generation
-      text_out <- reactive({
+      text_out <- shiny::reactive({
 
         new_order_text <- paste0('\"',input$rank_list_basic,'\"', collapse=',')
-        text_out_code <- str_c("    mutate(",quo_name(column_name)," = fct_relevel(",
-                               quo_name(column_name),", levels = ",  new_order_text,"))")
+        text_out_code <- stringr::str_c("    mutate(",dplyr::quo_name(column_name)," = fct_relevel(",
+                                        dplyr::quo_name(column_name),", levels = ",  new_order_text,"))")
 
         text_out <- paste(" %>%",text_out_code, sep="\n")
 
         # add convert to factor if data is character
         if(!is.factor(data %>%
-                      pull(var = !!column_name))){
-          fct_text <- str_c("    mutate(",quo_name(column_name)," = as_factor(",
-                            quo_name(column_name),")) %>%")
+                      dplyr::pull(var = !!column_name))){
+          fct_text <- stringr::str_c("    mutate(",dplyr::quo_name(column_name)," = as_factor(",
+                                     dplyr::quo_name(column_name),")) %>%")
           fct_text <- paste(" %>%",fct_text, sep="\n")
-          text_out <- paste(fct_text,str_replace(text_out," %>%\n",""),sep = "\n")
+          text_out <- paste(fct_text,stringr::str_replace(text_out," %>%\n",""),sep = "\n")
         }
 
         # output text
@@ -119,24 +114,24 @@ factor_sort <- function(data,col){
       })
 
       # code output
-      output$code <- renderText({
+      output$code <- shiny::renderText({
 
         text_out()
 
       })
 
       # copy text when copy button pressed
-      output$clip <- renderUI({
+      output$clip <- shiny::renderUI({
 
-        rclipButton(
+        rclipboard::rclipButton(
           inputId = "clipbtn",
           label = "Copy Code",
           clipText = text_out(),
-          icon = icon("clipboard")
+          icon = shiny::icon("clipboard")
         )
       })
 
-      observeEvent(input$close, stopApp())
+      shiny::observeEvent(input$close, shiny::stopApp())
     }
 
 
